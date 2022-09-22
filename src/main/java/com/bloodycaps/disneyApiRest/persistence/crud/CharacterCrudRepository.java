6450c21bf6a59eae45fc39ff22b673564e53ffba
Danilo Cortes <dacortes63@misena.edu.co>
2022-09-22 09:25:00 -0500

    chore: se crea las clases CharacterRepository y CharacterCrudRepository
    se crea la clase CharacterRepository y la interfaz CharacterCrudRepository dentro de la capa de la persistencia.

---
+++ src/main/java/com/bloodycaps/disneyApiRest/persistence/crud/CharacterCrudRepository.java
@@ -0,0 +1,7 @@
+package com.bloodycaps.disneyApiRest.persistence.crud;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CharacterCrudRepository extends CrudRepository<Character, Integer> {
+
+}
